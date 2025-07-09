beba34f353cec9ce647065f11e786f050d1afe01
David Leonardo Ramos Gutierrez <davidlrg89@gmail.com>
2025-07-09 17:57:59 -0500

    actualizo el setting

--- notas/storages.py
+++ notas/storages.py
@@ -4,8 +4,7 @@ from storages.backends.gcloud import GoogleCloudStorage
 class GoogleCloudMediaStorage(GoogleCloudStorage):
     """
     Configuración para los archivos multimedia públicos en Google Cloud Storage.
-    Estos archivos se guardan en la carpeta 'media/' de tu bucket.
+    La ruta completa del archivo ahora se define directamente en el modelo.
     """
-    location = 'media'
     default_acl = 'publicRead'
     file_overwrite = False
